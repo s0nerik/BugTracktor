af879dde5899f0dbbbb404a4f54b2dc3400d308d
sonerik <thesonerik@gmail.com>
2016-05-17 01:42:44 +0300

    getIssue only for project member.

--- api/controllers/issues.js
+++ api/controllers/issues.js
@@ -28,9 +28,13 @@ function createIssue(req, res) {
 }
 
 function getIssue(req, res) {
-  T.project_issues.get(req.swagger.params.projectId.value, req.swagger.params.issueIndex.value)
-                  .then(info => {
-                    console.log(info);
-                    res.json(info);
-                  });
+  T.project_members.check_member(req.user.id, req.swagger.params.projectId.value)
+                   .then(isMember => {
+                     if (isMember) {
+                       T.project_issues.get(req.swagger.params.projectId.value, req.swagger.params.issueIndex.value)
+                                       .then(info => res.json(info));
+                     } else {
+                       res.status(403).json({message: "You must be a project member to view its issues."});
+                     }
+                   });
 }
