099eba832fdd90bfad8e1af6ab442e3f41b20bea
sonerik <thesonerik@gmail.com>
2016-05-29 20:04:16 +0300

    getUserInfo request

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,6 +5,7 @@ var T = require('../helpers/sql/tables');
 module.exports = {
   newUser: newUser,
   getToken: getToken,
+  getUserInfo: getUserInfo,
 };
 
 function newUser(req, res) {
@@ -20,13 +21,17 @@ function newUser(req, res) {
 
 function getToken(req, res) {
   T.users.get_with_email(req.swagger.params.email.value)
-         .then(info => {
-           if (!info) {
+         .then(user => {
+           if (!user) {
              res.status(403).json({message: "User with a given email doesn't exist!"});
-           } else if (info.password == req.swagger.params.password.value) {
-             T.tokens.update_token_for_user_id(info.id).then(data => res.json(data));
+           } else if (user.password == req.swagger.params.password.value) {
+             T.tokens.update_token_for_user_id(user.id).then(token => res.json(token));
            } else {
              res.status(403).json({message: "Wrong password!"});
            }
          });
+}
+
+function getUserInfo(req, res) {
+  res.json(req.user);
 }
