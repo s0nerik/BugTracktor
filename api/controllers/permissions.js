9036991baa2b156f6ed3664b60ed20078b66b1fa
sonerik <thesonerik@gmail.com>
2016-05-29 03:32:55 +0300

    updated getPermissions

--- api/controllers/permissions.js
+++ api/controllers/permissions.js
@@ -8,5 +8,11 @@ module.exports = {
 
 function getPermissions(req, res) {
   T.permissions.get_by_user_id_and_project(req.user.id, req.swagger.params.projectId.value)
-               .then(info => res.json(info));
+               .then(info => {
+                 var result = [];
+                 for (var i in info) {
+                   result.push({name: info[i]});
+                 }
+                 res.json(result);
+               });
 }
